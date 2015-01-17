1a9a81916df951bbb06cf7bd3568c1f0d8c0669a
Hein Bekker <hein@netbek.co.za>
2015-01-17 17:18:07 +0200

    Fix demo

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,16 +1,30 @@
-(function () {
+/**
+ * AngularJS stopwatch demo
+ *
+ * @author Hein Bekker <hein@netbek.co.za>
+ * @copyright (c) 2015 Hein Bekker
+ * @license http://www.gnu.org/licenses/agpl-3.0.txt AGPLv3
+ */
+
+(function (window, angular, undefined) {
 	'use strict';
 
 	angular
 		.module('nb.stopwatch.demo', [
 			'nb.stopwatch'
 		])
-		.controller('MainController', function MainController () {
-		})
+		.controller('MainController', MainController)
 		.run(runBlock);
+
+	MainController.$inject = ['$scope', '$timeout'];
+	function MainController ($scope, $timeout) {
+		$timeout(function () {
+			$scope.$broadcast('nbStopwatch:start');
+		}, 500);
+	}
 
 	runBlock.$inject = ['AnimationFrame'];
 	function runBlock (AnimationFrame) {
 
 	}
-})();
+})(window, window.angular);
